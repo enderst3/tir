8cb2d484c61ae9d50ae3cd63a7c9478c297c915d
enderst3 <enderst3@gmail.com>
2017-09-27 20:02:34 -0700

    sorted and out of stock colored

--- src/App.js
+++ src/App.js
@@ -2,31 +2,45 @@ import React, { Component } from 'react'
 import './App.css'
 
 const products = [
-  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
   {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
   {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
   {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
   {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
   {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
 ]
+
+const compare = (recordA, recordB) => {
+  if (recordA.category > recordB.category) {
+    return -1
+  }
+  if (recordB.category > recordA.category) {
+    return 1
+  }
+  // a must be equal to b
+  return 0
+}
 
 let row = []
 let currentCategory = null
 
+products.sort(compare)
 for (var i = 0; i < products.length; i++) {
   let product = products[i]
   let category = product.category
+  currentCategory = category
   if (category !== currentCategory) {
     row.push(<tr><th colSpan='2'>{category}</th></tr>)
+  } if (product.stocked === false) {
+    row.push(<tr style={{color: 'red'}}><td>{product.name}</td><td>{product.price}</td></tr>)
   }
   row.push(<tr><td>{product.name}</td><td>{product.price}</td></tr>)
-  currentCategory = category
 }
 
 class App extends Component {
   render () {
     return (
-      <div className = 'productTable'>
+      <div className = 'inventoryApp'>
         <input
           type='text'
           placeholder='Search...' />
